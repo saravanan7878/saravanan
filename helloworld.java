07f338580891d4a433a4e50a52b1a7955be3b29a
saravanan7878 <saravanancloudcomputing@gmail.com>
2017-05-08 03:58:35 +0000

    helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+public static void main (String[] args )
+{
+    for (int i =0; i<=9; i++)
+      {
+        System.out.println("welcome to  jenkins");
+         }
+  }
+}
+
